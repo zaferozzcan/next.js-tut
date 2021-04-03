ce86107b8b8b41149813c076fc890b254a1f3080
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-03 14:00:20 -0400

    page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 export default function About() {
   return (
     <div>
       <h1>Index page </h1>
       <p> Index Page</p>
+      <Link as="/vehicle/zafer" href="/vehicle/[name]">
+        <button>Go to the zafer'car page</button>
+      </Link>
     </div>
   );
 }
